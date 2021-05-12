e89fee7f0884da741cf3b9ccbf6b636087b2a27f
Dor Levi <Dorlevi694@gmail.com>
2021-05-12 14:55:29 +0300

    first commit

---
+++ Models/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars_and_Manufacturers.Models.Entities
+{
+
+    public record Car(
+        int ModelYear,
+        string Divisiot, //make
+        string Carline, //model
+        double EngDispt, //  EngineDisplacement
+        int Cyl,  // Cylinders 
+        int CityFE,// City Fuel Efficiency
+        int HwyFE,// High Way Fuel Efficiency
+        int CombFE// Comp Fuel Efficiency
+    );
+
+}
---
+++ Models/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars_and_Manufacturers.Models.Entities
+{
+    public class User
+    {
+    }
+}
